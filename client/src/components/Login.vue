121110ad08f00aa9b4153aee8311a107e5ef4058
Dhayanandaskar <dhayanand.baskar@gmail.com>
2017-09-15 09:21:28 +0530

    Vuex state, password encyp, login token

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,27 +3,23 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
         <div class="pl-4 pr-4 pt-2 pb-2">
-           <form
-            name="tab-tracker-form"
-            autocomplete="off">
-            <v-text-field
-              label="Email"
-              v-model="email"
+           <v-text-field
+            label="Email"
+            v-model="email"
             ></v-text-field>
-            <br>
-            <v-text-field
-              label="Password"
-              type="password"
-              v-model="password"
-            ></v-text-field>
-           </form>
+          <br>
+          <v-text-field
+            label="Password"
+            type="password"
+            v-model="password"
+          ></v-text-field>
           <br>
           <div class="error" v-html="error" />
           <br>
-          <v-btn class="cyan" @click="register" dark>Register</v-btn>
+          <v-btn class="cyan" @click="login" dark>Login</v-btn>
         </div>
       </div>
     </v-flex>
@@ -33,7 +29,6 @@
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
 export default {
-  name: 'Register',
   data () {
     return {
       email: '',
@@ -42,7 +37,7 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
         const response = await AuthenticationService.login({
           email: this.email,
